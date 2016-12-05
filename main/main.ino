25f186c6e23b04041f9f61f2178d5096623c9a26
jcert <jgcertorio@uol.com.br>
2016-12-05 15:05:32 -0200

    init all

---
+++ main/main.ino
@@ -0,0 +1,28 @@
+//includes
+#include <IRremoteInt.h>
+#include <IRremote.h>
+#include <LedControl.h>
+
+
+//matriz 8x8
+
+
+
+//receptor ir
+//sensor de queda
+
+
+LedControl lc=LedControl(12,11,10,1); 
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+}
+
