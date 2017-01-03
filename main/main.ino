33f7a761cca22961c21c05202946dcb27baa7bef
jcert <jgcertorio@uol.com.br>
2017-01-03 20:39:03 -0200

    quando ativar cada mensagem no display, e por quanto tempo

--- main/main.ino
+++ main/main.ino
@@ -6,10 +6,12 @@
 LedControl lc=LedControl(12,11,10,1);//3 primeiros sao os pinos do spi e o ultimo eh quantos controladores estao ligados 
 bool mostra_terremoto;
 bool mostra_chama;
-#define tempo_display 10 //tempo, em segundos, após acabar o estado que ele mantem a a mensagem exibindo na tela 
+#define tempo_display_chama 		10 //tempo, em segundos, após acabar o estado que ele mantem na tela a mensagem de chama 
+#define tempo_display_terremoto 	10 //tempo, em segundos, após acabar o estado que ele mantem na tela a mensagem de terremoto 
 long stamp_display_chama;
 long stamp_display_terremoto;
 
+bool display_symbol[2]; //[0] se em chama, [1] se em terremoto
 
  
 //sensor de chama
@@ -19,15 +21,18 @@ long stamp_display_terremoto;
 //sensor de queda
 #define pin_queda 2
 #define tempo_sample 2 //em segundos o tempo de medidas
-#define alternacoes  2  //numero de vezes que ele deve trocar durante o tempo de sample
+#define limiar_trocas  2  //numero de vezes que ele deve trocar durante o tempo de sample
 int ultimo_estado; //qual o ultimo estado em que esteve a chave de tilt
 long ultimo_stamp;
 int trocas;        //
 
 bool ler_chama(){
   int ir_sentido = analogRead(pin_chama);
-  if(DEBUG) Serial.println(ir_sentido);  
-  return ir_sentido < limiar_chama;//quanto mais intensa a chama menor o valor do sensor
+  if(DEBUG){
+		Serial.print("ir: ");  
+		Serial.println(ir_sentido);  
+	}
+return ir_sentido < limiar_chama;//quanto mais intensa a chama menor o valor do sensor
   };
 
 void ler_oscila(){
@@ -36,13 +41,17 @@ void ler_oscila(){
 		if (ultimo_estado != nova_medida) trocas++;
    	ultimo_estado = nova_medida;
   	}else{
+  		if(DEBUG){
+			Serial.print("oscila: ");
+			Serial.println(trocas);	
+		} 
 		trocas = 0;		
 		ultimo_stamp = millis();
 	}
   };
 
 void se_oscilando_muito(){
-  if(trocas > alternacoes) mostra_terremoto = true;
+  if(trocas > limiar_trocas) mostra_terremoto = true;
   else mostra_terremoto = false;
 };
 
@@ -55,19 +64,23 @@ void display_chama(){
   if(mostra_chama){
     stamp_display_chama = millis();
     //mostra o simbolo de chama
-    }
-  if((stamp_display_chama+tempo_display*1000)<millis()){
+    display_symbol[0] = true;
+	}
+  if((stamp_display_chama+tempo_display_chama*1000)<millis()){
     //apaga o display   
-  }
+    display_symbol[0] = false;
+  	}
 };
 
 void display_terremoto(){
   if(mostra_terremoto){
     stamp_display_terremoto = millis();
-    //mostra o simbolo de chama
+    //mostra o simbolo de terremoto
+    display_symbol[1] = true;
     }
-  if((stamp_display_terremoto+tempo_display*1000)<millis()){
+  if((stamp_display_terremoto+tempo_display_terremoto*1000)<millis()){
     //apaga o display   
+    display_symbol[1] = false;
   }
 };
   
@@ -88,6 +101,8 @@ void loop() {
 
   display_terremoto();
   display_chama();
+
+  display_refresh();//talvez colocar por interrupcao
   delay(300);
 
 }
